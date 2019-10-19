3be2567716d275e61d28afff3692ccde3859bcf7
Marino Salazar <marinosalazar@gmail.com>
2019-10-19 01:17:46 -0400

    this code its to create a simple db on mongodb using python

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from pymongo import MongoClient #import modules
 
-MONGO_URI = 'mongodb://localhost' #the uri for mongo client
+MONGO_URI = 'mongodb://localhost:27017' #the uri for mongo client where mongodb means protocol, localgost server ip, and 20717 th default port
 
 client = MongoClient(MONGO_URI) 
 
-db = client['mycustomers'] # createa database
-collection = db['products'] # createa a collection
+db = client['smartcontracts'] # createa database you can use whatever you want after you put some data on this
+collection = db['customers'] # createa a collection in the db
 
-collection.insert_one({"name": "Api", "price": 300})
+
+
+collection.insert_one({"name": "Marino Salazar"}) #this way is to insert a single item on the collection and can ceate a db
